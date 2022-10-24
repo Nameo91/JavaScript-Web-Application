b449a6d13fb7afab32a7bdc3d02e306b0003f447
Na-Rae Kim <narae1096@gmail.com>
2022-10-24 15:42:25 +0100

    exercise of build tool

---
+++ resources/example-1/index.js
@@ -0,0 +1,13 @@
+// console.log doesn't print anything on the page
+// it is not meant to be visible to the user, but for you
+// to help in debugging and getting visibility in your JS code.
+//
+// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
+const add = require('./add');
+const multiply = require('./multiply');
+
+const sum = multiply(add(3,3), 3);
+
+console.log(sum);
+
+
