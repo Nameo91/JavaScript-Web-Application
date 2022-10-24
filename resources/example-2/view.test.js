3a2376de282f2761fbc2e20fdab34be271261428
Na-Rae Kim <narae1096@gmail.com>
2022-10-24 21:47:15 +0100

    exercise of testing DOM

---
+++ resources/example-2/view.test.js
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs');
+const View = require('./view');
+
+describe('Page view', () => {
+  it('displays 2 paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new View();
+
+    expect(document.querySelectorAll('p').length).toBe(2);
+  });
+
+  it('adds a new paragraph', () => {
+    // Arrange
+    document.body.innterHTML = fs.readFileSync('./index.html');
+    const view = new View();
+    // Act
+    view.addParagraph();
+
+    //Assert
+    expect(document.querySelectorAll('p').length).toBe(3);
+  })
+
+  it('deletes all paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+    view.clearParagraphs();
+    
+    expect(document.querySelectorAll('p').length).toBe(0);
+  })
+});
